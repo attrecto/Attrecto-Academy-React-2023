6d236f9ebb52ff220fb9e36bd3064f5ce7ae6acc
BotiD3 <nagybotond123@gmail.com>
2023-09-25 00:03:33 +0200

    lover than zero error msg

--- src/App.tsx
+++ src/App.tsx
@@ -4,42 +4,51 @@ import Navbar from "./components/navbar/Navbar";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [loverThanZeroError, setloverThanZeroError] =useState(false);
 
   const updateCounter = (increase: boolean) => {
     setCounter((currentValue) => {
+      if (currentValue == 0 && !increase)
+      {
+        setloverThanZeroError(true);
+        return 0;
+      }
+      setloverThanZeroError(false);
       return increase ? currentValue + 1 : currentValue - 1;
     });
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
-      <div className="container d-flex justify-content-center">
-        <div className="card bg-white shadow text-center p-4 m-4">
-          <h5>Counter: {counter}</h5>
-          <div className="d-flex justify-content-center flex-wrap gap-2">
-            <button
-              className="btn btn-primary"
-              onClick={() => updateCounter(true)}
-            >
-              Increase +
-            </button>
-            <button
-              className="btn btn-secondary"
-              onClick={() => updateCounter(false)}
-            >
-              Decrease -
-            </button>
-            <button className="btn btn-danger" onClick={() => setCounter(0)}>
-              Reset
-            </button>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+        </header>
+        <div className="container d-flex justify-content-center">
+          <div className="card bg-white shadow text-center p-4 m-4">
+            <h5>Counter: {counter}</h5>
+            <h6 className="text-danger">{loverThanZeroError ? "Counter can't be lover than zero" : ""}</h6>
+            <div className="d-flex justify-content-center flex-wrap gap-2">
+              <button
+                  className="btn btn-primary"
+                  onClick={() => updateCounter(true)}
+              >
+                Increase +
+              </button>
+              <button
+                  className="btn btn-secondary"
+                  onClick={() => updateCounter(false)}
+              >
+                Decrease -
+              </button>
+              <button className="btn btn-danger" onClick={() => setCounter(0)}>
+                Reset
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
+
 
 export default App;
